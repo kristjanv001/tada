ac262248e72c62aae447edd346e9cc0a5a419949
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2020-09-07 00:01:57 +0300

    added show date feature

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const todoListUl = document.getElementById("todo-list-ul");
 const todoAddWrapper = document.getElementById("todo-add-wrapper");
 const todoBtn = document.getElementById("todo-btn");
+const todoInput = document.getElementById("todo-input");
+const dateHeader = document.getElementById("date");
 
 const todos = [];
 
 let newTodoText = "";
+
+console.log(dateHeader.innerText);
+
+function getDateAndDisplayIt() {
+  const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const date = new Date();
+
+  dateHeader.innerText = `${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()}`;
+}
 
 function addTodoToDOM(e) {
   e.preventDefault();
@@ -15,19 +42,25 @@ function addTodoToDOM(e) {
     </label>
     <a class="deleteBtn">🞨</a>
     </li>`;
-  todoListUl.insertAdjacentHTML("afterbegin", todoItem);
-  document.getElementById("todo-input").value = "";
+  if (todoInput.value) {
+    todoListUl.insertAdjacentHTML("afterbegin", todoItem);
+    todoInput.value = "";
+  }
 }
 
 function checkOrDelete(e) {
   if (e.target.className === "deleteBtn")
     e.target.parentNode.parentNode.removeChild(e.target.parentNode);
 
-  if (e.target.type === "checkbox" && e.target.checked)
+  if (e.target.type === "checkbox" && e.target.checked) {
     e.target.nextSibling.nextSibling.style.textDecoration = "line-through";
+    e.target.nextSibling.nextSibling.style.color = "gray";
+  }
 
-  if (e.target.type === "checkbox" && !e.target.checked)
+  if (e.target.type === "checkbox" && !e.target.checked) {
     e.target.nextSibling.nextSibling.style.textDecoration = "none";
+    e.target.nextSibling.nextSibling.style.color = "";
+  }
 }
 
 function loadEventListeners() {
@@ -36,6 +69,7 @@ function loadEventListeners() {
 }
 
 loadEventListeners();
+getDateAndDisplayIt();
 
 // function addTodo(text) {
 //   todos.push({
@@ -44,15 +78,3 @@ loadEventListeners();
 //     done: false,
 //   });
 // }
-
-// todoAddWrapper.addEventListener("keyup", (e) => {
-//   newTodoText = e.target.value;
-// });
-
-// todoAddWrapper.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   if (newTodoText) {
-//     addTodoToDOM(newTodoText);
-//     document.getElementById("todo-input").value = "";
-//   }
-// });
